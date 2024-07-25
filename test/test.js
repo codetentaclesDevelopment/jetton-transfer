9a699db8934dfb11253ea89ed6ec4b84ae491a8d
ajaycodetentacles <148066702+ajaycodetentacles@users.noreply.github.com>
2024-07-25 17:16:10 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 const { sendJetton } = require('../dist/index');
 
-const mnemonic = "candy elite ability tumble miracle luggage energy budget pluck snap engage season neglect uniform bamboo sausage guess uncover rebel narrow option obtain path jeans";
+const mnemonic = "Your memonic";
 const fromWallet = "EQCKM7NhPW8y7Bg5wSpxMeEtkyaoxZUPXKZTYr2GnH_Tg_5d";
 const toWallet = "0QDTdoOcmN44KWTToHvfQzZYzZHP4n3k9R5rEaRGc5fFXWOM";
 const amount = "1"; // Amount in TON (will be converted to nano)
